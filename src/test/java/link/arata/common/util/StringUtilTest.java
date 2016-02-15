6af8bd339b2e754950277cf5f36ebd3840d0e619
webarata3 <webarata3@gmail.com>
2016-02-15 19:49:39 +0900

    StringUtil.trimRightの追加

--- src/test/java/link/arata/common/util/StringUtilTest.java
+++ src/test/java/link/arata/common/util/StringUtilTest.java
@@ -54,4 +54,49 @@ public class StringUtilTest {
     public void trimLeftの右側がtrimされないこと() {
         assertThat(StringUtil.trimLeft("あいうえお\n\t　 "), is("あいうえお\n\t　 "));
     }
+
+    public void trimLeftですべての文字がtrim対象の場合() {
+        assertThat(StringUtil.trimLeft(" 　\t\n"), is(""));
+    }
+
+    public void trimLeftで空文字の場合() {
+        assertThat(StringUtil.trimLeft(""), is(""));
+    }
+
+    @Test
+    public void trimRightの半角ブランクがtrimされること() {
+        assertThat(StringUtil.trimRight("あいうえお "), is("あいうえお"));
+    }
+
+    @Test
+    public void trimRight全角ブランクがtrimされること() {
+        assertThat(StringUtil.trimRight("あいうえお　"), is("あいうえお"));
+    }
+
+    @Test
+    public void trimRightのタブがtrimされること() {
+        assertThat(StringUtil.trimRight("あいうえお\t"), is("あいうえお"));
+    }
+
+    @Test
+    public void trimRightの改行がtrimされること() {
+        assertThat(StringUtil.trimRight("あいうえお\n"), is("あいうえお"));
+    }
+
+    @Test
+    public void trimRightのいろいろ混ざった場合にtrimされること() {
+        assertThat(StringUtil.trimRight("あいうえお 　\t\n"), is("あいうえお"));
+    }
+
+    public void trimRightの左側がtrimされないこと() {
+        assertThat(StringUtil.trimRight("\n\t　 あいうえお"), is("あいうえお\n\t　 "));
+    }
+
+    public void trimRightですべての文字がtrim対象の場合() {
+        assertThat(StringUtil.trimRight(" 　\t\n"), is(""));
+    }
+
+    public void trimRightで空文字の場合() {
+        assertThat(StringUtil.trimRight(""), is(""));
+    }
 }
