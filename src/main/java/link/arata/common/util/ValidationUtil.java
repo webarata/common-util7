2d3d9dce3222b48a3d0b7a3d46a10ad442ddf2f1
webarata3 <webarata3@gmail.com>
2016-02-17 15:14:49 +0900

    ValidationUtil.requiredの追加

--- src/main/java/link/arata/common/util/ValidationUtil.java
+++ src/main/java/link/arata/common/util/ValidationUtil.java
@@ -1,5 +1,6 @@
 package link.arata.common.util;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 /**
@@ -9,13 +10,43 @@ import javax.annotation.Nullable;
  *
  */
 public class ValidationUtil {
+    private static TrimType DEFAULT_TRIM_TYPE = TrimType.BOTH;
 
-    public static boolean required(@Nullable String target) {
+    /**
+     * デフォルトのTrim方向を決める。明示しない場合には、このTrimが使用される。
+     * 
+     * @param defaultTrim
+     */
+    public static void setDefaultTrim(@Nonnull TrimType defaultTrimType) {
+        DEFAULT_TRIM_TYPE = defaultTrimType;
+    }
+
+    /**
+     * 必須チェックをする
+     * 
+     * @param target
+     *            検査する文字列
+     * @param trimType
+     *            検査前に行うtrimの種類
+     * @return 空文字列かnullの場合false
+     */
+    public static boolean required(@Nullable String target, @Nonnull TrimType trimType) {
         if (target == null) {
             return false;
         }
-        String trimTarget = StringUtil.trimRight(target);
+        String trimTarget = trimType.trim(target);
 
         return StringUtil.isNotEmpty(trimTarget);
+    }
+
+    /**
+     * デフォルトのTrimTypeを使用した必須チェック
+     * 
+     * @param target
+     *            検査する文字列
+     * @return 空文字列かnullの場合false
+     */
+    public static boolean required(@Nullable String target) {
+        return required(target, DEFAULT_TRIM_TYPE);
     }
 }
