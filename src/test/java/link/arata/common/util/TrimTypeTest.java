f8f753ff34d50890de85e6c9ba5e459ad10f431d
webarata3 <webarata3@gmail.com>
2016-02-17 13:31:55 +0900

    TrimTypeTestのメソッド名の変更

--- src/test/java/link/arata/common/util/TrimTypeTest.java
+++ src/test/java/link/arata/common/util/TrimTypeTest.java
@@ -7,22 +7,22 @@ import org.junit.Test;
 
 public class TrimTypeTest {
     @Test
-    public void trimType_NONEのテスト() {
+    public void trimType_NONEの場合() {
         assertThat(TrimType.NONE.trim(" あいうえお "), is(" あいうえお "));
     }
 
     @Test
-    public void trimType_BOTHのテスト() {
+    public void trimType_BOTHの場合() {
         assertThat(TrimType.BOTH.trim(" あいうえお "), is("あいうえお"));
     }
 
     @Test
-    public void trimType_LEFTのテスト() {
+    public void trimType_LEFTの場合() {
         assertThat(TrimType.LEFT.trim(" あいうえお "), is("あいうえお "));
     }
 
     @Test
-    public void trimType_RIGHTのテスト() {
+    public void trimType_RIGHTの場合() {
         assertThat(TrimType.RIGHT.trim(" あいうえお "), is(" あいうえお"));
     }
 }
