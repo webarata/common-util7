97abbfbf1a94d0c96020576e5918508d732af27b
webarata3 <webarata3@gmail.com>
2016-02-19 18:45:42 +0900

    引数名の変更

--- src/main/java/link/arata/common/util/StringUtil.java
+++ src/main/java/link/arata/common/util/StringUtil.java
@@ -12,15 +12,15 @@ public class StringUtil {
     /**
      * 文字列がnullか空文字列かをチェックする
      * 
-     * @param target
+     * @param value
      *            検査する文字列
      * @return 文字列がnullもしくは空文字列の場合true
      */
-    public static boolean isEmpty(@Nullable String target) {
-        if (target == null) {
+    public static boolean isEmpty(@Nullable String value) {
+        if (value == null) {
             return true;
         }
-        if (target.length() == 0) {
+        if (value.length() == 0) {
             return true;
         }
         return false;
@@ -29,62 +29,62 @@ public class StringUtil {
     /**
      * 文字列がnullや空文字でないことをチェックする
      * 
-     * @param target
+     * @param value
      *            検査する文字列
      * @return 文字列がnullもしくは空文字列でない場合true
      */
-    public static boolean isNotEmpty(@Nullable String target) {
-        return !isEmpty(target);
+    public static boolean isNotEmpty(@Nullable String value) {
+        return !isEmpty(value);
     }
 
     /**
      * 文字列の左側の空白とそれに準ずる文字の連続を取り除く
      * 
-     * @param target
+     * @param value
      *            対象の文字列
      * @return 左側をtrimした結果の文字列
      */
     @Nonnull
-    public static String trimLeft(@Nonnull String target) {
+    public static String trimLeft(@Nonnull String value) {
         int i = 0;
-        for (; i < target.length(); i++) {
-            if (!Character.isWhitespace(target.charAt(i))) {
+        for (; i < value.length(); i++) {
+            if (!Character.isWhitespace(value.charAt(i))) {
                 break;
             }
         }
 
-        return target.substring(i);
+        return value.substring(i);
     }
 
     /**
      * 文字列の右側の空白とそれに準ずる文字の連続を取り除く
      * 
-     * @param target
+     * @param value
      *            対象の文字列
      * @return 右側をtrimした結果の文字列
      */
     @Nonnull
-    public static String trimRight(@Nonnull String target) {
-        int i = target.length() - 1;
+    public static String trimRight(@Nonnull String value) {
+        int i = value.length() - 1;
         for (; i >= 0; i--) {
-            if (!Character.isWhitespace(target.charAt(i))) {
+            if (!Character.isWhitespace(value.charAt(i))) {
                 break;
             }
         }
 
-        return target.substring(0, i + 1);
+        return value.substring(0, i + 1);
     }
 
     /**
      * 文字列の両端の空白とそれに準ずる文字の連続を取り除く
      * 
-     * @param target
+     * @param value
      *            対象の文字列
      * @return 右側をtrimした結果の文字列
      */
     @Nonnull
-    public static String trim(@Nonnull String target) {
-        String trimString = trimLeft(target);
-        return trimRight(trimString);
+    public static String trim(@Nonnull String value) {
+        String trimValue = trimLeft(value);
+        return trimRight(trimValue);
     }
 }
