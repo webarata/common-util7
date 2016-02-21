e4f28b0e991333cb144a359cbcf44748fe0bcf41
webarata3 <webarata3@gmail.com>
2016-02-21 14:04:26 +0900

    ValidationUtilのlengthチェックで改行コードの正規化を行う

--- src/test/java/link/arata/common/util/ValidationUtilTest.java
+++ src/test/java/link/arata/common/util/ValidationUtilTest.java
@@ -67,6 +67,21 @@ public class ValidationUtilTest {
     }
 
     @Test
+    public void minLengthであCRLFあで2文字以上の場合() {
+        assertThat(ValidationUtil.minLength("あ\r\nあ", 2), is(true));
+    }
+
+    @Test
+    public void minLengthであCRLFあで3文字以上の場合() {
+        assertThat(ValidationUtil.minLength("あ\r\nあ", 3), is(true));
+    }
+
+    @Test
+    public void minLengthであCRLFあで4文字以上の場合() {
+        assertThat(ValidationUtil.minLength("あ\r\nあ", 4), is(false));
+    }
+
+    @Test
     public void minLengthで𠮷野家で2文字以上の場合() {
         assertThat(ValidationUtil.minLength("𠮷野家", 2), is(true));
     }
@@ -94,59 +109,74 @@ public class ValidationUtilTest {
     }
 
     @Test
-    public void maxLengthであああで2文字以上の場合() {
+    public void maxLengthであああで2文字以下の場合() {
         assertThat(ValidationUtil.maxLength("あああ", 2), is(false));
     }
 
     @Test
-    public void maxLengthであああで3文字以上の場合() {
+    public void maxLengthであああで3文字以下の場合() {
         assertThat(ValidationUtil.maxLength("あああ", 3), is(true));
     }
 
     @Test
-    public void maxLengthであああで4文字以上の場合() {
+    public void maxLengthであああで4文字以下の場合() {
         assertThat(ValidationUtil.maxLength("あああ", 4), is(true));
     }
 
     @Test
-    public void maxLengthで半角ブランクあああ半角ブランクで2文字以上の場合() {
+    public void maxLengthで半角ブランクあああ半角ブランクで2文字以下の場合() {
         assertThat(ValidationUtil.maxLength(" あああ ", 2), is(false));
     }
 
     @Test
-    public void maxLengthで半角ブランクあああ半角ブランクで3文字以上の場合() {
+    public void maxLengthで半角ブランクあああ半角ブランクで3文字以下の場合() {
         assertThat(ValidationUtil.maxLength(" あああ ", 3), is(true));
     }
 
     @Test
-    public void maxLengthで半角ブランクあああ半角ブランクで4文字以上の場合() {
+    public void maxLengthで半角ブランクあああ半角ブランクで4文字以下の場合() {
         assertThat(ValidationUtil.maxLength(" あああ ", 4), is(true));
     }
 
     @Test
-    public void maxLengthで𠮷野家で2文字以上の場合() {
+    public void maxLengthであCRLFあで2文字以上の場合() {
+        assertThat(ValidationUtil.maxLength("あ\r\nあ", 2), is(false));
+    }
+
+    @Test
+    public void maxLengthであCRLFあで3文字以下の場合() {
+        assertThat(ValidationUtil.maxLength("あ\r\nあ", 3), is(true));
+    }
+
+    @Test
+    public void maxLengthであCRLFあで4文字以下の場合() {
+        assertThat(ValidationUtil.maxLength("あ\r\nあ", 4), is(true));
+    }
+
+    @Test
+    public void maxLengthで𠮷野家で2文字以下の場合() {
         assertThat(ValidationUtil.maxLength("𠮷野家", 2), is(false));
     }
 
     @Test
-    public void maxLengthで𠮷野家で3文字以上の場合() {
+    public void maxLengthで𠮷野家で3文字以下の場合() {
         assertThat(ValidationUtil.maxLength("𠮷野家", 3), is(true));
     }
 
     @Test
-    public void maxLengthで𠮷野家で4文字以上の場合() {
+    public void maxLengthで𠮷野家で4文字以下の場合() {
         assertThat(ValidationUtil.maxLength("𠮷野家", 4), is(true));
     }
 
-    public void maxLengthで半角ブランク3つでtrimせず2文字以上の場合() {
+    public void maxLengthで半角ブランク3つでtrimせず2文字以下の場合() {
         assertThat(ValidationUtil.maxLength("   ", 2, LineBreakType.CRLF, TrimType.NONE), is(false));
     }
 
-    public void maxLengthで半角ブランク3つでtrimせず3文字以上の場合() {
+    public void maxLengthで半角ブランク3つでtrimせず3文字以下の場合() {
         assertThat(ValidationUtil.maxLength("   ", 3, LineBreakType.CRLF, TrimType.NONE), is(true));
     }
 
-    public void maxLengthで半角ブランク3つでtrimせず4文字以上の場合() {
+    public void maxLengthで半角ブランク3つでtrimせず4文字以下の場合() {
         assertThat(ValidationUtil.maxLength("   ", 4, LineBreakType.CRLF, TrimType.NONE), is(true));
     }
 }
