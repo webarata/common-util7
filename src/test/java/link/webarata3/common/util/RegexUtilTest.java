bcdc6e7a4ce548a4c9dc66227b0544df87b52dc7
webarata3 <webarata3@gmail.com>
2016-11-03 17:31:08 +0900

    リファクタリング

--- src/test/java/link/webarata3/common/util/RegexUtilTest.java
+++ src/test/java/link/webarata3/common/util/RegexUtilTest.java
@@ -6,43 +6,43 @@ import static org.junit.Assert.*;
 import java.util.regex.Pattern;
 
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
 
-import link.webarata3.common.util.RegexUtil;
-
+@RunWith(Enclosed.class)
 public class RegexUtilTest {
     @Test
-    public void getPatternでキャッすされること() {
+    public void getPatternでキャッシュされること() {
         Pattern pattern = RegexUtil.getPattern(RegexUtil.EM_KATAKANA);
         assertThat(RegexUtil.getPattern(RegexUtil.EM_KATAKANA), is(pattern));
     }
 
-    @Test
-    public void findで全角カタカナでアイウエオの場合() {
-        assertThat(RegexUtil.perfectMatch(RegexUtil.EM_KATAKANA, "アイウエオ"), is(true));
-    }
+    @RunWith(Theories.class)
+    public static class 全角カタカナのテスト {
+        @DataPoints
+        public static Fixture[] PARAMs = { new Fixture("アイウエオ", true), new Fixture("あいうえお", false),
+                new Fixture("\u30A0", true), // 全角カタカナで一番最初の文字
+                new Fixture("\u309F", false), // 全角カタカナで1番最初の文字の1つ前の文字
+                new Fixture("\u30FF", true), // 全角カタカナで一番最後の文字
+                new Fixture("\u3100", false) // 全角カタカナで1番最後の文字の1つ後の文字
+        };
 
-    @Test
-    public void findで全角カタカナであいうえおの場合() {
-        assertThat(RegexUtil.perfectMatch(RegexUtil.EM_KATAKANA, "あいうえお"), is(false));
-    }
+        @Theory
+        public void test(Fixture fixture) {
+            assertThat(RegexUtil.perfectMatch(RegexUtil.EM_KATAKANA, fixture.str), is(fixture.expected));
+        }
 
-    @Test
-    public void findで全角カタカナで一番最初の文字の場合() {
-        assertThat(RegexUtil.perfectMatch(RegexUtil.EM_KATAKANA, "\u30A0"), is(true));
-    }
+        static class Fixture {
+            String str;
+            boolean expected;
 
-    @Test
-    public void findで全角カタカナで一番最初の文字の1つ前の文字場合() {
-        assertThat(RegexUtil.perfectMatch(RegexUtil.EM_KATAKANA, "\u309F"), is(false));
-    }
-
-    @Test
-    public void findで全角カタカナで一番最後の文字の場合() {
-        assertThat(RegexUtil.perfectMatch(RegexUtil.EM_KATAKANA, "\u30FF"), is(true));
-    }
-
-    @Test
-    public void findで全角カタカナで一番最後の文字の1つ後の文字場合() {
-        assertThat(RegexUtil.perfectMatch(RegexUtil.EM_KATAKANA, "\u3100"), is(false));
+            Fixture(String str, boolean expected) {
+                this.str = str;
+                this.expected = expected;
+            }
+        }
     }
 }
