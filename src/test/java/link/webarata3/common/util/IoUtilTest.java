aab9f1385ca2986a8ee0ac82a50d32eeb0da042f
webarata3 <webarata3@gmail.com>
2016-11-03 17:06:02 +0900

    リファクタリング

--- src/test/java/link/webarata3/common/util/IoUtilTest.java
+++ src/test/java/link/webarata3/common/util/IoUtilTest.java
@@ -9,37 +9,37 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
+import org.junit.After;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
-import link.webarata3.common.util.IoUtil;
 
 public class IoUtilTest {
 
     private static String BASE_DIR;
 
     @BeforeClass
-    public static void setup() {
-        try {
-            File file = new File(IoUtil.class.getResource("IoUtilSrc.txt").toURI());
-            BASE_DIR = file.getParent();
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
+    public static void setUpClass() throws URISyntaxException {
+        File file = new File(IoUtil.class.getResource("IoUtilSrc.txt").toURI());
+        BASE_DIR = file.getParent();
+    }
+
+    @After
+    public void tearDown() {
+        new File(BASE_DIR + File.separator + "IoUtilDest.txt").delete();
     }
 
     @Test
     public void copyFileのファイル名の場合のテスト() throws IOException {
         IoUtil.copyFile(BASE_DIR + File.separator + "IoUtilSrc.txt", BASE_DIR + File.separator + "IoUtilDest.txt");
+
         assertThat(new File(BASE_DIR + File.separator + "IoUtilDest.txt").exists(), is(true));
-        new File(BASE_DIR + File.separator + "IoUtilDest.txt").delete();
     }
 
     @Test
     public void copyFileのstreamの場合テスト() throws IOException {
         IoUtil.copyFile(new FileInputStream(new File(BASE_DIR, "IoUtilSrc.txt")),
                 new FileOutputStream(new File(BASE_DIR, "IoUtilDest.txt")));
+
         assertThat(new File(BASE_DIR + File.separator + "IoUtilDest.txt").exists(), is(true));
-        new File(BASE_DIR + File.separator + "IoUtilDest.txt").delete();
     }
 }
